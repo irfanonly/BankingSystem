709965da763d3126047e6058144da2dc70bd3dd2
Irfan <mamirfan92@gmail.com>
2024-10-11 10:41:26 +0400

    Refactor

--- BankingSystem/Data/Transfer.cs
+++ BankingSystem/Data/Transfer.cs
@@ -10,13 +10,4 @@ namespace BankingSystem.Data
         public DateTime TransferredOn { get; set; } = DateTime.UtcNow;
         public ICollection<TransferTransaction> TransferTransactions { get; set; } = new List<TransferTransaction>();
     }
-
-    public class TransferTransaction
-    {
-        public int TransferId { get; set; }
-        public virtual Transfer Transfer { get; set; }
-        public long TransactionId { get; set; }
-
-        public virtual Transaction Transaction { get; set; }
-    }
 }
---
+++ BankingSystem/Data/TransferTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSystem.Data
+{
+    public class TransferTransaction
+    {
+        public int TransferId { get; set; }
+        public virtual Transfer Transfer { get; set; }
+        public long TransactionId { get; set; }
+
+        public virtual Transaction Transaction { get; set; }
+    }
+}
